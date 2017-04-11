a4fccc12b855af85671f5fc2e4cba21d2cef6d3b
Liseth <cardozoliseth@gmail.com>
2017-04-10 22:59:47 -0700

    complete problem 1 and 2

--- lib/01_book_titles.rb
+++ lib/01_book_titles.rb
@@ -1,3 +1,10 @@
 class Book
-  # TODO: your code goes here!
+  attr_reader :title
+  def title=(title)
+    litle_words = ["a", "an", "in", "the", "and", "over", "on", "of"]
+    title = title.split.map.with_index do |word, idx|
+      litle_words.include?(word) && idx != 0 ? word : word.capitalize
+    end
+    @title = title.join(" ")
+  end
 end
